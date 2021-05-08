ed3996efd44dbd03fb0a104807528117e04a70c5
Skyteks <DanielEmig@gmx.de>
2021-05-09 01:25:38 +0200

    Add NavMeshComponents

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float followSpeed;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref cameraFollowVelocity, Time.deltaTime / followSpeed);
+    }
+}
