8cf5936cc15e6e47bcd9d2532160afdbd7afe42b
Skyteks <DanielEmig@gmx.de>
2021-05-13 05:56:28 +0200

    Fixed questionmark rotation

---
+++ Assets/Scripts/IndependantRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class IndependantRotator : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector3 axis = Vector3.up;
+    private float currentAngle;
+
+    void LateUpdate()
+    {
+        currentAngle = (currentAngle + speed * Time.deltaTime) % 360f;
+        float parentAngle = transform.parent.rotation.y;
+        transform.rotation = Quaternion.Euler(Vector3.up * (parentAngle - currentAngle));
+    }
+}
